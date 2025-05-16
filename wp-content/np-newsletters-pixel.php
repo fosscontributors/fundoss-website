6cf94654730c9856f6599e756084d1c6c75bda28
Gus Austin <gusaus@gmail.com>
2025-05-16 23:53:44 +0000

    updates via dashboard

--- wp-content/np-newsletters-pixel.php
+++ wp-content/np-newsletters-pixel.php
@@ -17,7 +17,7 @@
 		if ( ! isset( $_GET["id"] ) || ! isset( $_GET["tid"] ) || ! isset( $_GET["em"] ) ) {
 			exit;
 		}
-		$file = "/files/2024/10/newspack_newsletters_pixel_log_WJYtDl";
+		$file = "/files/2025/05/newspack_newsletters_pixel_log_uwyopf";
 		$id = $_GET["id"];
 		$tid = $_GET["tid"];
 		$email_address = $_GET["em"];
