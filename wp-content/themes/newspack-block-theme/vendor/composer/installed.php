dcb0488cda070bbac0fb68bafdfb9bd80662d2b5
Gus Austin <gusaus@gmail.com>
2025-06-20 17:48:55 -0700

    newspack theme updates

---
+++ wp-content/themes/newspack-block-theme/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/newspack-block-theme',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => '14ae6255b6a5a5b8e89414342d913207006f4db3',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/newspack-block-theme' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => '14ae6255b6a5a5b8e89414342d913207006f4db3',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
